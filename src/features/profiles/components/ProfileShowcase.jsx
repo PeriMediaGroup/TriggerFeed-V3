5016d75a5a116fc8b187a454c1232d3c26a51199
Pete Schroeder <pete.schroeder@protonmail.com>
2026-09-13 09:42:33 -0400

    remove

--- src/features/profiles/components/ProfileShowcase.jsx
+++ src/features/profiles/components/ProfileShowcase.jsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Crosshair } from "lucide-react";
-import FoundingMemberBadge from "@/features/profiles/components/FoundingMemberBadge";
 
 export default function ProfileShowcase({ topFriends = [], topGuns = [] }) {
   return (
@@ -59,16 +58,8 @@ export default function ProfileShowcase({ topFriends = [], topGuns = [] }) {
 
                     <div className="profile-showcase__identity">
                       <div className="profile-showcase__name">{displayName}</div>
-                      <FoundingMemberBadge
-                        number={friend?.founding_member_number}
-                        className="profile-showcase__founding-badge"
-                      />
                     </div>
                   </Link>
-                  <FoundingMemberBadge
-                    number={friend?.founding_member_number}
-                    className="profile-showcase__founding-badge"
-                  />
                 </li>
               );
             })}
